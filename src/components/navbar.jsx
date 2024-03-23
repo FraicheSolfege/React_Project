d6d3524f7221248478d8d2f95370dcbb9681e547
CodeWithDustin <codewithdustin@gmail.com>
2024-03-23 15:47:48 -0500

    Update movies working sorta, need to figure out how to not have to refetch the data

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,21 +1,37 @@
 import * as React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
-function NavBar(){
+function NavBar() {
   return (
-      <nav className="navbar">
-          <ul>
-              <li><Link to="/home">Home</Link></li>
-              <li><Link to="/comingsoon">Coming Soon</Link></li>
-              <li><Link to="/classics">Classics</Link></li>
-              <li><Link to="/trending">Trending</Link></li>
-              <li><Link to="/recommendations">Recommendations</Link></li>
-              <li><Link to="/newreleases">New Releases</Link></li>
-              <li><Link to="/search">Search</Link></li>
-              <li><Link to="/contact">Contact Us</Link></li>
-          </ul>
-      </nav>
-  )
+    <nav className="navbar">
+      <ul>
+        <li>
+          <Link to="/home">Home</Link>
+        </li>
+        <li>
+          <Link to="/comingsoon">Coming Soon</Link>
+        </li>
+        <li>
+          <Link to="/classics">Classics</Link>
+        </li>
+        <li>
+          <Link to="/trending">Trending</Link>
+        </li>
+        <li>
+          <Link to="/recommendations">Recommendations</Link>
+        </li>
+        <li>
+          <Link to="/newreleases">New Releases</Link>
+        </li>
+        <li>
+          <Link to="/updateMovie">Update Movies</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact Us</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
-export default NavBar
+export default NavBar;
